13e44b1ee1b1b6c93a8d1b90e4e29faafd660e1b
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-20 22:34:38 +0545

    writesync

--- file.js
+++ file.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 fs.writeFileSync("read.txt", "welcome to channel ");
+fs.writeFileSync("read.txt", "welcome to channel  anish pokharel!");
 
 // const name = "anish";
 // console.log(name);
