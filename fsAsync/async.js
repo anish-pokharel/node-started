95a9a887c0d19b976ffe6e071cc6384e6e6dbc59
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-23 01:05:43 +0545

    append

--- fsAsync/async.js
+++ fsAsync/async.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
 
-fs.writeFile(
-  "read.txt",
-  "today is awesome day",
+// fs.writeFile(
+//   "read.txt",
+//   "today is awesome day",
 
-  (err) => {
-    console.log("completed");
-    console.log(err);
-  }
-);
+//   (err) => {
+//     console.log("completed");
+//     console.log(err);
+//   }
+// );
+
+fs.appendFile("read.txt", "hello i am anish", (err) => {
+  console.log("data completed");
+  console.log(err);
+});
