111ed7c0195850e51bf10b2e428bbda5420a1584
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-23 00:21:41 +0545

    delete sectio

--- index1.js
+++ index1.js
@@ -3,5 +3,7 @@ const fs = require("fs");
 // fs.mkdirSync("thapa");
 // fs.writeFileSync("thapa/bio.txt", "my name is anish pokharel ");
 
-const data = fs.readFileSync("thapa/bio.txt", "utf-8");
-console.log(data);
+// const data = fs.readFileSync("thapa/bio.txt", "utf-8");
+// console.log(data);
+
+fs.unlinkSync("thapa/bio.txt");
