c4d9377a9fd1e928ea6ba05c5ae941adaeee5bbb
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-22 23:29:31 +0545

    renamethrough fs sync

--- file.js
+++ file.js
@@ -3,14 +3,16 @@ const fs = require("fs");
 // fs.writeFileSync("read.txt", "welcome to channel ");
 // fs.writeFileSync("read.txt", "welcome to channel  anish pokharel!");
 
-fs.appendFileSync("read.txt", "how are you are you fine");
+// fs.appendFileSync("read.txt", "how are you are you fine");
 // fs.writeFileSync("rackvideo.txt", "15.31");
 
-const buf_data = fs.readFileSync("read.txt");
-console.log(buf_data);
+// const buf_data = fs.readFileSync("read.txt");
+// console.log(buf_data);
 
-org_data = buf_data.toString();
-console.log(org_data);
+// org_data = buf_data.toString();
+// console.log(org_data);
+
+fs.renameSync("read.txt", "readwrite.txt");
 
 // const name = "anish";
 // console.log(name);
