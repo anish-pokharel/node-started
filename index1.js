a9a1acecd491034e1dd0d04acc2a273b189a9dc2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-23 00:19:33 +0545

    utf-8 encoding

--- index1.js
+++ index1.js
@@ -1,1 +1,7 @@
-fs;
+const fs = require("fs");
+
+// fs.mkdirSync("thapa");
+// fs.writeFileSync("thapa/bio.txt", "my name is anish pokharel ");
+
+const data = fs.readFileSync("thapa/bio.txt", "utf-8");
+console.log(data);
