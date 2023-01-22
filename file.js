4ac04ec07ede5c3e8dfce1aed700926b26eb9dbc
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-22 23:04:32 +0545

    error chang

--- file.js
+++ file.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
 // fs.writeFileSync("read.txt", "welcome to channel ");
-fs.writeFileSync("read.txt", "welcome to channel  anish pokharel!");
+// fs.writeFileSync("read.txt", "welcome to channel  anish pokharel!");
 
-// fs.appendFileSync("read.txt", "how are you are you fine");
+fs.appendFileSync("read.txt", "how are you are you fine");
 // fs.writeFileSync("rackvideo.txt", "15.31");
 
 // const name = "anish";
