0e505b8fc271e8611eaa7939ba3f3a4eff3b7a58
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-22 23:07:46 +0545

    buffer

--- file.js
+++ file.js
@@ -6,5 +6,8 @@ const fs = require("fs");
 fs.appendFileSync("read.txt", "how are you are you fine");
 // fs.writeFileSync("rackvideo.txt", "15.31");
 
+const buf_data = fs.readFileSync("read.txt");
+console.log(buf_data);
+
 // const name = "anish";
 // console.log(name);
