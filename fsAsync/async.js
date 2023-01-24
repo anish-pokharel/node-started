a638b9a4d4af909d2220251158231be107a11d5a
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-24 23:31:27 +0545

    today learn

--- fsAsync/async.js
+++ fsAsync/async.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 
-// fs.writeFile(
-//   "read.txt",
-//   "today is awesome day",
+fs.writeFile(
+  "read.txt",
+  "today is awesome day 24 jan",
 
-//   (err) => {
-//     console.log("completed");
-//     console.log(err);
-//   }
-// );
+  (err) => {
+    console.log("completed");
+    console.log(err);
+  }
+);
 
 // fs.appendFile("read.txt", "hello i am anish", (err) => {
 //   console.log("data completed");
 //   console.log(err);
 // });
 
-// fs.readFile("read.txt", "utf-8", (err, data) => {
-//   console.log("read file");
+// fs.readFile("fsAsync/read.txt", "utf-8", (err, data) => {
+//   // console.log("read file");
 //   console.log(data);
+
 // });
 
-fs.readFile("read.txt", "utf-8", (err, data) => {
-  console.log(data);
-  // console.log(err);
-});
+// fs.readFile("read.txt", "utf-8", (err, data) => {
+//   console.log(data);
+//   // console.log(err);
+// });
