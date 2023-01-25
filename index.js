9918ec7eacdae437a74c74800c4acff3d9e92815
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-25 23:57:43 +0545

    folder created

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 // js expression use variable ultiline code use _ to get last result  editiior mode
 const fs = require("fs");
 
-fs.mkdir("thapa", (err) => {
-  console.log("folder created");
-});
+// fs.mkdir("thapa", (err) => {
+//   console.log("folder created");
+// });
+
+fs.writeFile("./thapa/bio.txt ", "my name is anish pokharel");
