900a3accd88e8fd159126953ab1369dcc0800f9c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-25 23:53:58 +0545

    challange

--- fsAsync/async.js
+++ fsAsync/async.js
@@ -50,9 +50,9 @@
 // console.log(data);
 // console.log("after the data");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.readFile("read.txt", "utf-8", (err, data) => {
-  console.log(" data");
-});
-console.log("after the data");
+// fs.readFile("read.txt", "utf-8", (err, data) => {
+//   console.log(" data");
+// });
+// console.log("after the data");
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-console.log("wewlcome to my site");
+// console.log("wewlcome to my site");
 // comleted till3
 // npm
 
 // repl
 
 // js expression use variable ultiline code use _ to get last result  editiior mode
+const fs = require("fs");
