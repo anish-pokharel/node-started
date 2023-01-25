c6d409a365ce40aa65a495add1bc1ff931ce80c4
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-25 23:56:40 +0545

    folder created

--- index.js
+++ index.js
@@ -6,4 +6,7 @@
 
 // js expression use variable ultiline code use _ to get last result  editiior mode
 const fs = require("fs");
-fs.mkdir(thapa);
+
+fs.mkdir("thapa", (err) => {
+  console.log("folder created");
+});
