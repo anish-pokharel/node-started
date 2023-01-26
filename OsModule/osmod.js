2a20c858969c8581650c07a4ec9ecdf027a1b4a6
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-27 01:05:41 +0545

    all fle st up

--- OsModule/osmod.js
+++ OsModule/osmod.js
@@ -1,14 +1,14 @@
-const os = require("os");
+// const os = require("os");
 
-console.log(os.arch());
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.tmpdir());
-console.log(os.type());
-const freeMemory = os.freemem();
-console.log(freeMemory);
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.tmpdir());
+// console.log(os.type());
+// const freeMemory = os.freemem();
+// console.log(freeMemory);
 
-console.log(`${freeMemory / 1024 / 1024 / 1024}`);
+// console.log(`${freeMemory / 1024 / 1024 / 1024}`);
 
-const totalMemory = os.totalmem();
-console.log(`${totalMemory / 1024 / 1024 / 1024}`);
+// const totalMemory = os.totalmem();
+// console.log(`${totalMemory / 1024 / 1024 / 1024}`);
