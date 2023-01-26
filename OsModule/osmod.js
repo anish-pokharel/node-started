cadb95887bb812df167803e3111973b39ed53483
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-26 23:50:31 +0545

    os completed

--- OsModule/osmod.js
+++ OsModule/osmod.js
@@ -3,6 +3,8 @@ const os = require("os");
 console.log(os.arch());
 console.log(os.hostname());
 console.log(os.platform());
+console.log(os.tmpdir());
+console.log(os.type());
 const freeMemory = os.freemem();
 console.log(freeMemory);
 
