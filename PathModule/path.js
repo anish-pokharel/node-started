33389310313798ae8b3e2ecb08f901c79d1a9f2c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-27 01:14:30 +0545

    parse path module

--- PathModule/path.js
+++ PathModule/path.js
@@ -2,3 +2,5 @@ const path = require("path");
 console.log(path.dirname("D:/node/PathModule/path.js"));
 console.log("ext name is :");
 console.log(path.extname("D:/node/PathModule/path.js"));
+console.log(path.basename("D:/node/PathModule/path.js"));
+console.log(path.parse("D:/node/PathModule/path.js"));
