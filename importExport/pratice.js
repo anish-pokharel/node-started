d895fa8f6f0f0903333c712f3be9a2b10dbaec1b
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-30 22:47:06 +0545

    module

--- importExport/pratice.js
+++ importExport/pratice.js
@@ -4,7 +4,11 @@ const divide = (a, b) => {
 // const nam = () => {
 //   return "anish pokharel";
 // };
+const multiple = (a, b, c) => {
+  return a * b + c;
+};
 
 module.exports.divide = divide;
+module.exports.multiple = multiple;
 // module.exports.nam = nam;
 // module.exports.myDatetime = myDatetime;
