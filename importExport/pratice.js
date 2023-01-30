f7d5f693b5e49d311f51bda213cdf2c841c0259b
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-30 22:42:01 +0545

    6th push console log

--- importExport/pratice.js
+++ importExport/pratice.js
@@ -4,10 +4,7 @@ const divide = (a, b) => {
 // const nam = () => {
 //   return "anish pokharel";
 // };
-const nam = "anish pokharel";
-const myDateTime = () => {
-  return Date;
-};
-// module.exports.divide = divide;
-module.exports.nam = nam;
-module.exports.myDatetime = myDatetime;
+
+module.exports.divide = divide;
+// module.exports.nam = nam;
+// module.exports.myDatetime = myDatetime;
