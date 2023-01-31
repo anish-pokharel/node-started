5a5fe0a3f4681db7faba90023efe1a9cd88d95a2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-31 11:15:10 +0545

    multiple odule call

--- importExport/index.js
+++ importExport/index.js
@@ -1,15 +1,15 @@
-const oper = require("./oper");
-const oper1 = require("./pratice");
+const { add, name, sub, mult, divi } = require("./oper");
+const { divide, multiple } = require("./pratice");
 // const fs = require("fs");
 // const sub = require("./oper");
 // const name = require("./oper");
-console.log(oper);
-console.log(oper.name);
-console.log(oper.add(5, 5));
-console.log(oper.sub(15, 5));
-console.log(oper.mult(2, 7));
-console.log(oper.divi(100, 2));
-console.log(oper1.divide(1000, 2500));
+// console.log(oper);
+console.log(name);
+console.log(add(5, 5));
+console.log(sub(15, 5));
+console.log(mult(2, 7));
+console.log(divi(100, 2));
+console.log(divide(1000, 2500));
 
-console.log(oper1.multiple(2, 3, 1));
-console.log(oper1.multiple(2, 3, 3));
+console.log(multiple(2, 3, 1));
+console.log(multiple(2, 3, 3));
