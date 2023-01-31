612a7c1e6708ffce06860909e8de32a61e1d27a0
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-31 13:20:14 +0545

    code

--- npm package/index.js
+++ npm package/index.js
@@ -1,2 +1,6 @@
 const chalk = require("chalk");
 console.log(chalk.red.inverse("failed"));
+console.log("hello");
+console.log(chalk.yellow.inverse("sucess"));
+console.log("hello");
+console.log(chalk.blue.inverse("pendig"));
