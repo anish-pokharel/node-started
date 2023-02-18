0af0f07428dcc756909e6e885c7004bf4d5d4ac8
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-18 22:00:01 +0545

    node

--- PathModule/path.js
+++ PathModule/path.js
@@ -1,11 +1,19 @@
 const path = require("path");
-// console.log(path.dirname("D:/node/PathModule/path.js"));
-// console.log("ext name is :");
-// console.log(path.extname("D:/node/PathModule/path.js"));
-// console.log(path.basename("D:/node/PathModule/path.js"));
-// console.log(path.parse("D:/node/PathModule/path.js"));
+console.log(path.dirname("D:/node/PathModule/path.js"));
+console.log("ext name is :");
+console.log(path.extname("D:/node/PathModule/path.js"));
+console.log(path.basename("D:/node/PathModule/path.js"));
+console.log(path.parse("D:/node/PathModule/path.js"));
 
-// const myPath = path.parse("D:/node/PathModule/path.js");
-// console.log(myPath.root);
+const myPath = path.parse("D:/node/PathModule/path.js");
+console.log(myPath.root);
+console.log(path.dirname("D:/node/PathModule/path.js"));
+console.log("ext name is :");
+console.log(path.extname("D:/node/PathModule/path.js"));
+console.log(path.basename("D:/node/PathModule/path.js"));
+console.log(path.parse("D:/node/PathModule/path.js"));
+
+const myPath = path.parse("D:/node/PathModule/path.js");
+console.log(myPath.root);
 
 console.log(path.dirname("D:/node/PathModule/path.js"));
