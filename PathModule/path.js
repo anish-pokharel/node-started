d900f00193086abdde10224a4a1da5a8f1b4decf
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-18 22:01:41 +0545

    node

--- PathModule/path.js
+++ PathModule/path.js
@@ -7,13 +7,5 @@ console.log(path.parse("D:/node/PathModule/path.js"));
 
 const myPath = path.parse("D:/node/PathModule/path.js");
 console.log(myPath.root);
-console.log(path.dirname("D:/node/PathModule/path.js"));
-console.log("ext name is :");
-console.log(path.extname("D:/node/PathModule/path.js"));
-console.log(path.basename("D:/node/PathModule/path.js"));
-console.log(path.parse("D:/node/PathModule/path.js"));
-
-const myPath = path.parse("D:/node/PathModule/path.js");
-console.log(myPath.root);
 
 console.log(path.dirname("D:/node/PathModule/path.js"));
