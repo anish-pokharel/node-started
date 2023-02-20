8bf8f190611d39f9e5c53ba5b96c257f1e03549a
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-20 23:38:50 +0545

    1st

--- importExport/oper.js
+++ importExport/oper.js
@@ -13,11 +13,29 @@ const mult = (a, b) => {
 const divi = (a, b) => {
   return a / b;
 };
+const sub = (a, b) => {
+  return a - b;
+};
+const mult = (a, b) => {
+  return a * b;
+};
+const divi = (a, b) => {
+  return a / b;
+};
+const sub = (a, b) => {
+  return a - b;
+};
+const mult = (a, b) => {
+  return a * b;
+};
+const divi = (a, b) => {
+  return a / b;
+};
 
-// module.exports.divi1 = divi;
-// module.exports.add = add;
-// module.exports.sub = sub;
-// module.exports.mult = mult;
-// module.exports.name = name;
+module.exports.divi1 = divi;
+module.exports.add = add;
+module.exports.sub = sub;
+module.exports.mult = mult;
+module.exports.name = name;
 
 module.exports = { add, sub, mult, divi, name };
