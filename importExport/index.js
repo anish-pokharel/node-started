5f46732189577d7166be1156a8f7802fd7ad15b3
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-28 23:29:38 +0545

    recoding life

--- importExport/index.js
+++ importExport/index.js
@@ -13,18 +13,3 @@ console.log(divide(1000, 2500));
 
 console.log(multiple(2, 3, 1));
 console.log(multiple(2, 3, 3));
-const { add, name, sub, mult, divi } = require("./oper");
-const { divide, multiple } = require("./pratice");
-// const fs = require("fs");
-// const sub = require("./oper");
-// const name = require("./oper");
-// console.log(oper);
-console.log(name);
-console.log(add(5, 5));
-console.log(sub(15, 5));
-console.log(mult(2, 7));
-console.log(divi(100, 2));
-console.log(divide(1000, 2500));
-
-console.log(multiple(2, 3, 1));
-console.log(multiple(2, 3, 3));
