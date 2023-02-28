291231b13a123894ccbec0eaeb0e1287c8cafbfc
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-28 23:27:40 +0545

    recoding life

--- importExport/oper.js
+++ importExport/oper.js
@@ -21,3 +21,26 @@ module.exports.mult = mult;
 module.exports.name = name;
 
 module.exports = { add, sub, mult, divi, name };
+const add = (a, b) => {
+  return a + b;
+};
+
+const name = "anish pokharel";
+
+const sub = (a, b) => {
+  return a - b;
+};
+const mult = (a, b) => {
+  return a * b;
+};
+const divi = (a, b) => {
+  return a / b;
+};
+
+module.exports.divi1 = divi;
+module.exports.add = add;
+module.exports.sub = sub;
+module.exports.mult = mult;
+module.exports.name = name;
+
+module.exports = { add, sub, mult, divi, name };
