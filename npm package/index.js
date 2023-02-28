01f4d4a4d3cbaa418893b033428ac279ffc3d1f6
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-02-28 23:28:37 +0545

    recoding life

--- npm package/index.js
+++ npm package/index.js
@@ -7,3 +7,12 @@ console.log(chalk.blue.inverse("pendig"));
 console.log(
   chalk.blue("Hello") + " World" + chalk.red("anish ") + chalk.green("pokharel")
 );
+const chalk = require("chalk");
+console.log(chalk.red.inverse("failed"));
+console.log("hello");
+console.log(chalk.yellow.inverse("sucess"));
+console.log("hello");
+console.log(chalk.blue.inverse("pendig"));
+console.log(
+  chalk.blue("Hello") + " World" + chalk.red("anish ") + chalk.green("pokharel")
+);
